ddf5b49cbb842f16af97e3ae1908e46ce9eba481
DuTss <emduthy@gmail.com>
2020-12-03 05:36:45 -0500

    app pierre feuille ciseau

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Choix utilisateur
-//let userChoice = prompt("Choisissez entre papier, ciseau, caillou");
+let userChoice = prompt("Choisissez entre papier, ciseau, caillou");
 
 // Choix de l'ordinateur
 let computerChoice = Math.random()
@@ -15,13 +15,37 @@ if (computerChoice < 0.34) {
 //console.log(computerChoice);
 
 var compare = function(choice1, choice2) {
-    
+    if (choice1 === choice2) {
+        return "Egalité"
+    }
+    else if (choice1 === "caillou") {
+        if (choice2 === "ciseau") {
+            return "caillou gagne"
+        } else if (choice2 === "papier") {
+            return "papier gagne"
+        }
+    }
+    else if (choice1 === "papier") {
+        if (choice2 === "ciseau") {
+            return "ciseau gagne"
+        } else if (choice2 === "caillou") {
+            return "papier gagne"
+        }
+    }
+    else if (choice1 === "ciseau") {
+        if (choice2 === "papier") {
+            return "ciseau gagne"
+        } else if (choice2 === "caillou") {
+            return "caillou gagne"
+        }
+    }
 }
-//console.log(compare(computerChoice,userChoice));
+
+console.log(compare(computerChoice,userChoice));
 
 /*var sandwich = function(pain,viande,legumes,sauce) {
     return `Votre sandwich sera composé de: ${pain},${viande},${legumes},${sauce}`
-}*/
+}
 var sandwich = function(params1,params2,params3,params4) {
     return `Votre sandwich sera composé de: ${params1},${params2},${params3},${params4}`
 }
@@ -29,4 +53,4 @@ var parisien = sandwich("pain complet","jambon","salade","ketchup")
 var vendeen = sandwich("pain de mie","saucisse",["salade","tomate","radis"],"Pas de sauce")
 
 console.log("menu parisien:",parisien);
-console.log("menu vendeen:",vendeen);
+console.log("menu vendeen:",vendeen);*/
